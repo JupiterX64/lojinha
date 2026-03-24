07f0defb78cb84da4a5e0622b784fc2c745d8973
JupiterX64 <giuliano.dev@outlook.com>
2026-03-24 12:36:20 -0300

    Inicializando o repositório com os arquivos iniciais do projeto

---
+++ src/main/java/br/com/lojinha/LojinhaApp.java
@@ -0,0 +1,10 @@
+package br.com.lojinha;
+
+public class LojinhaApp {
+    public static void main(String[] args) {
+        String joia = "100gr de ouro 50k";
+        int minhaIdade = 43;
+        double meuSalario = 5000.00;
+        boolean minhaResposta = true;
+    }
+}
