481da9c1be2c370fee9b254b0a74b0e106bf412b
JupiterX64 <giuliano.dev@outlook.com>
2026-03-25 20:45:59 -0300

    Criação da classe Produto

--- src/main/java/br/com/lojinha/LojinhaApp.java
+++ src/main/java/br/com/lojinha/LojinhaApp.java
@@ -1,10 +1,17 @@
 package br.com.lojinha;
 
+import br.com.lojinha.pojos.Produto;
+
 public class LojinhaApp {
     public static void main(String[] args) {
-        String joia = "100gr de ouro 50k";
-        int minhaIdade = 43;
-        double meuSalario = 5000.00;
-        boolean minhaResposta = true;
+        Produto meuProduto = new Produto();
+
+        meuProduto.nome = "Playstation 5";
+        meuProduto.valor = 4999.99;
+        meuProduto.marca = "Sony";
+        meuProduto.tamanho = "Médio";
+        meuProduto.itensInclusos = "2 Controles e 3 Jogos";
+
+        System.out.println(meuProduto.nome);
     }
 }
